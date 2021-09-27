729bf5f06136039bdf7bff0d983dbfe79b152f7b
douglasbaltazar <douglasbaltazar1@gmail.com>
2021-09-27 20:25:53 -0300

    Cards OK

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsLists from './ProductsLists';
 import StepHeaders from './StepHeaders';
 import './styles.css'
+import { Product } from './types';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepHeaders />
+            <ProductsLists products={products}/>
         </div>
     )
 }
